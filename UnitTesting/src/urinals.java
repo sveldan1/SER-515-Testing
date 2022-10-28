df97c6d2f8758bac83c0c0b691243c203ab3a6f3
sveldan1 <sveldan1@asu.edu>
2022-10-27 17:32:25 -0700

    Added first skeleton function for isValidString

--- UnitTesting/src/urinals.java
+++ UnitTesting/src/urinals.java
@@ -1,6 +1,13 @@
+import java.util.*;
+
 //AUTHOR: SOWMYA VELDANDI
 public class urinals {
+    public static boolean isValidString(String s){
+        System.out.println("Not yet implemented");
+        return true;
+    }
     public static void main(String[] args) {
+
 
     }
 }
