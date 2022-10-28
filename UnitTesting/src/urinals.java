b6d3a9f768c77a158b9610c74df466d2d5fc1289
sveldan1 <sveldan1@asu.edu>
2022-10-27 20:30:37 -0700

    Implemented taking input from file

--- UnitTesting/src/urinals.java
+++ UnitTesting/src/urinals.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 //AUTHOR: SOWMYA VELDANDI
 public class urinals {
+    public static  int RULE_NO=0;
     public static boolean isStringValid(String s){
         for(int i=0;i<s.length()-1;i++){
             if(s.charAt(i)=='1' && s.charAt(i+1)=='1'){
@@ -14,6 +15,32 @@ public class urinals {
 
         }
         return true;
+    }
+
+    static File createFile(){
+        String s = "rule"+RULE_NO+".txt";
+        if(RULE_NO==0){
+            s="rule.txt";
+        }
+        File file=new File(s);
+        while (file.exists()){
+            RULE_NO++;
+            return createFile();
+        }
+        return file;
+    }
+
+     static boolean writeintoFile(String s, File f){
+        try{
+        FileWriter writer = new FileWriter(f);
+        BufferedWriter bw = new BufferedWriter(writer);
+        bw.write(s);
+        bw.close();
+        RULE_NO++;}
+        catch (Exception e){
+            return false;
+        }
+        return  true;
     }
    static int countUrinals(String s){
         if(isStringValid(s)){
@@ -42,14 +69,46 @@ public class urinals {
 
 
         Scanner sc= new Scanner(System.in);
-        System.out.println("Enter a string: ");
+        System.out.println("Enter 0 if input is given from keyboard or Enter 1 if the input is given from input file");
+        int a=Integer.parseInt(sc.nextLine().trim());
+        if(a==0){
+            System.out.println("Enter string");
         String s= sc.nextLine();
         ///System.out.print("You have entered: "+s);
-        if(isStringValid(s)){System.out.println("valid");}
-        else{
-            System.out.println("not valid");
-        }
+//        if(isStringValid(s)){System.out.println("valid");}
+//        else{
+//            System.out.println("not valid");
+//        }
+
        int  n= countUrinals(s);
         System.out.println(n);
+    } else if (a==1) {
+            StringBuilder ot=new StringBuilder();
+            File f = new File("src/urinals.dat");
+            FileReader fr =new FileReader(f);
+            if(fr==null){
+                System.out.println("urinals.dat file is empty");
+            }
+            BufferedReader br = new BufferedReader(fr);
+            String str;
+            while ((str=br.readLine())!=null){
+                if(isStringValid(str)){
+                    int n=countUrinals(str);
+                    ot.append(n);
+                    ot.append("\n");
+                }
+                else{
+                    ot.append("-1");
+                    ot.append("\n");
+                }
+            }
+
+           System.out.println(writeintoFile(ot.toString(),createFile()));
+
+
+        }
+        else{
+            System.out.println("Input is wrong");
+        }
     }
 }
